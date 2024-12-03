0ab0f849ffc8d91693dc07109daf7cb58b50c6c2
tohamy <ahmadtohamy@icloud.com>
2024-12-03 20:51:19 +0200

    init

--- src/components/Mainfooter.jsx
+++ src/components/Mainfooter.jsx
@@ -3,7 +3,7 @@ import Mainlayout from '../layout/Mainlayout'
 export default function Mainfooter() {
   return (
   <nav style={{display:'flex', justifyContent:"center"
-  }}> get
+  }}> 
     <link to="/">home</link>
     <link to="/login">login</link>
   </nav>
